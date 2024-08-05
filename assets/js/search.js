5e5a9b9bf31ea697c5067c0c737cb7a25edea0d0
Taha Daoud <taha.daoud@sivogroup.com>
2024-08-05 13:56:46 +0100

    ajouter recherche au site

---
+++ assets/js/search.js
@@ -0,0 +1,60 @@
+// search.js
+
+(function() {
+    function displayResults(results, store) {
+      var searchResults = document.getElementById('search-results');
+  
+      if (results.length) { // Are there any results?
+        searchResults.innerHTML = '';
+  
+        for (var i = 0; i < results.length; i++) { // Iterate over the results
+          var item = results[i];
+          var ref = item.ref;
+          var title = store[ref].title;
+          var url = store[ref].url;
+          
+          var li = document.createElement('li');
+          var a = document.createElement('a');
+          a.href = url;
+          a.textContent = title;
+          li.appendChild(a);
+          searchResults.appendChild(li);
+        }
+      } else {
+        searchResults.innerHTML = '<li>Aucun résultat trouvé</li>';
+      }
+    }
+  
+    function getSearchIndex() {
+      var idx = lunr(function () {
+        this.field('title');
+        this.field('content');
+        this.ref('id');
+      });
+  
+      return fetch('/search.json')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          data.forEach(function(doc) {
+            idx.add(doc);
+          });
+          return { idx: idx, store: data };
+        });
+    }
+  
+    var searchInput = document.getElementById('search-input');
+    if (searchInput) {
+      getSearchIndex().then(function(data) {
+        var idx = data.idx;
+        var store = data.store;
+  
+        searchInput.addEventListener('input', function() {
+          var query = searchInput.value;
+          var results = idx.search(query);
+          displayResults(results, store);
+        });
+      });
+    }
+  })();  
--- search.md
+++ search.md
@@ -10,5 +10,4 @@ title: "Recherche"
   <ul id="search-results"></ul>
 </form>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lunr.js/2.3.9/lunr.min.js"></script>
 <script src="{{ "/assets/js/search.js" | relative_url }}"></script>
